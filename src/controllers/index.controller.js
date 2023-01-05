2c78763248d9017042ca563f74ca373b3b0f9748
Carlos López <68704651+CralpCode@users.noreply.github.com>
2023-01-05 06:41:19 +0000

    detalles

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,11 +1,11 @@
 import { pool } from "../db.js";
 
 export const ping = async (req ,res) => {
-    /* try { */
+    try { 
         const [result] =  await pool.query('SELECT "Pong" AS result')
         console.log(result)
         res.json(result[0])
-    /* } catch (error) {
+    } catch (error) {
         res.send('error');
-    } */
+    } 
 }
